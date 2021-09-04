4960b3ae30f708debcf8f400bfd328264e4bee35
ANCHAL <palspecify752@gmail.com>
2021-09-04 18:11:42 +0530

    changed arrays to arraylist

--- SolutionJava.java
+++ SolutionJava.java
@@ -1,16 +1,20 @@
 
-import java.io.*;
-
-class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+class SolutionJava{
 	public static void main (String[] args) {
-		int[] array={2,3,4,10,40};
-		int ValueToFind=10;
+		ArrayList<Integer>arr = new ArrayList<Integer>() ;
+		arr.add(2) ;
+		arr.add(3) ;
+		arr.add(4) ;
+		arr.add(10) ;
+		arr.add(10) ;
 		
 		// Here we'll create two pointers 
 		//start and end which will point to 1st and last element's indexes respectively 
-		
+		int ValueToFind = 10 ;
 		int start=0;
-		int end=array.length-1; //5
+		int end=arr.size()-1; //5
 		
 		while(start<=end){
 		    
@@ -18,7 +22,7 @@ class Main {
 		    int mid=start+(end-start)/2;
 		    
 		    //if our mid index element is target element we'll print mid
-		    if(array[mid]==ValueToFind){
+		    if(arr.get(mid)==ValueToFind){
 		        System.out.print("Index = "+mid);
 		        break;
 		        //here we don't need to check further so we break from loop
@@ -26,7 +30,7 @@ class Main {
 		    
 		    //if our mid index element is greater than target then it’s sure that our target would be before mid element
 		    //so we set end as mid-1
-		    else if(array[mid]>ValueToFind){
+		    else if(arr.get(mid)>ValueToFind){
 		        end=mid-1;
 		    }
 		    
